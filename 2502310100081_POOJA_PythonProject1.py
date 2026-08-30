ccc8eabd0294cf7ce5ba293115b62375f9f99504
Pooja <pk8854764@gmail.com>
2026-08-30 17:55:43 +0530

    Delete 2502310100081_POOJA_PythonProject1.py

--- 2502310100081_POOJA_PythonProject1.py
+++
@@ -1,2 +0,0 @@
-# SmartCampus_Utility_Project
-A Python-based Smart-Campus Utility &amp; Access Pass Generator that calculates access  fees ,merit discounts , parking charge  , surcharge , and electricity bill.
